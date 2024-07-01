ebcfb184104674683e9f119eff29b62612b25f3c
paliacci <paliacci@aliyun.com>
2024-07-01 15:36:44 +0800

    Update default_pipeline.py
    尝试修复奔溃问题

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -63,7 +63,7 @@ def assert_model_integrity():
 @torch.inference_mode()
 def refresh_base_model(name):
     global model_base
-    #"""
+    """
     if name not in models:
         rel_filename = os.path.abspath(os.path.realpath(os.path.join(modules.config.path_checkpoints, name)))
         abs_filename = os.path.join('/root/autodl-tmp/models/',name)
@@ -103,10 +103,13 @@ def refresh_base_model(name):
     if model_base.filename == filename:
          return
 
-    model_base = core.StableDiffusionModel()
+    # model_base = core.StableDiffusionModel()
     model_base = core.load_model(filename)
     print(f'Base model loaded: {model_base.filename}')
-    """
+
+
+    
+    #"""
     return
 
 
