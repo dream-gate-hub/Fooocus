70c5fd674c08ac4e547385bb643e64957094ac3e
Andrew <paliacci@aliyun.com>
2024-08-14 23:11:47 +0800

    photo to text length

--- extras/interrogate.py
+++ extras/interrogate.py
@@ -17,8 +17,7 @@ blip_repo_root = os.path.join(os.path.dirname(__file__), 'BLIP')
 class Interrogator:
     def __init__(self):
         self.blip_model = None
-        self.load_device = torch.device('cpu')
-        self.offload_device = torch.device('cpu')
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.dtype = torch.float32
 
     @torch.no_grad()
@@ -35,29 +34,28 @@ class Interrogator:
                                  med_config=os.path.join(blip_repo_root, "configs", "med_config.json"))
             model.eval()
 
-            self.load_device = model_management.text_encoder_device()
-            self.offload_device = model_management.text_encoder_offload_device()
-            self.dtype = torch.float32
-
-            model.to(self.offload_device)
-
-            if model_management.should_use_fp16(device=self.load_device):
+            if model_management.should_use_fp16(device=self.device):
                 model.half()
                 self.dtype = torch.float16
 
-            self.blip_model = ModelPatcher(model, load_device=self.load_device, offload_device=self.offload_device)
+            model.to(self.device)
 
-        model_management.load_model_gpu(self.blip_model)
+            self.blip_model = ModelPatcher(model, load_device=self.device, offload_device=self.device)
+
+        # model_management.load_model_gpu(self.blip_model)
 
         gpu_image = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((blip_image_eval_size, blip_image_eval_size), interpolation=InterpolationMode.BICUBIC),
             transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-        ])(img_rgb).unsqueeze(0).to(device=self.load_device, dtype=self.dtype)
+        ])(img_rgb).unsqueeze(0).to(self.device, dtype=self.dtype)
 
-        caption = self.blip_model.model.generate(gpu_image, sample=True, num_beams=1, max_length=75)[0]
+        caption = self.blip_model.model.generate(gpu_image, sample=True, num_beams=1, min_length=20, max_length=150)[0]
 
         return caption
+        # return 0
 
 
 default_interrogator = Interrogator().interrogate
+
+
